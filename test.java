d4760dff2e56d49fb94f719b08caf5d7d592699c
Prajwal <01fe22bca003@kletech.ac.in>
2024-03-11 15:08:50 +0530

    Main Program

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test extends extended_cal{
+    public static void main(String[]args)
+    {
+        test t1 = new test();
+        System.out.println("Square root = "+t1.square(100));
+        System.out.println("Cube root = "+t1.cube(50));
+    }    
+}
